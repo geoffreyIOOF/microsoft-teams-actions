e396e4790c6fa947eae8ed6b5a03f2c3d6ebc63e
Geoffrey <geoffrey.thomazeau@ioof.com.au>
2021-12-02 11:54:24 +1000

    Update MessageHandler.cs

--- src/GitHubActions.Teams.ConsoleApp/MessageHandler.cs
+++ src/GitHubActions.Teams.ConsoleApp/MessageHandler.cs
@@ -40,6 +40,7 @@ namespace Aliencube.GitHubActions.Teams.ConsoleApp
                 Summary = ParseString(options.Summary),
                 Text = ParseString(options.Text),
                 ThemeColor = ParseString(options.ThemeColor),
+                Msteams = ParseCollection<Msteams>(options.Msteams, settings),
                 Sections = ParseCollection<Section>(options.Sections, settings),
                 Actions = ParseCollection<BaseAction>(options.Actions, settings)
             };
